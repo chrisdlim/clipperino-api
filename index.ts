a6a0d2c90c9b2f8892bb5846754ae88ad55768bf
christophlim <22702896+chrisdlim@users.noreply.github.com>
2022-05-18 22:53:49 -0400

    Lamdafied

--- index.ts
+++ index.ts
@@ -2,34 +2,39 @@ import fastify from 'fastify';
 import { GetClips, UploadClip } from './src/actions';
 import { S3Service } from './src/services';
 
-const app = fastify();
-app.register(require('@fastify/multipart'));
 const port = 9000;
-const clipsBucketName = 'clips';
+const init = () => {
+  const app = fastify();
+  app.register(require('@fastify/multipart'));
+  const clipsBucketName = 'clips';
 
-const s3Service = new S3Service();
+  const s3Service = new S3Service();
 
-app.get('/health', async (req, reply) => {
-  reply.statusCode = 200;
-  reply.send();
-});
+  app.get('/health', async (req, reply) => {
+    reply.statusCode = 200;
+    reply.send();
+  });
 
-app.get('/clips', async (req, reply) => {
-  const query = new GetClips(s3Service, clipsBucketName);
-  reply.send(await query.handle());
-});
+  app.get('/clips', async (req, reply) => {
+    const query = new GetClips(s3Service, clipsBucketName);
+    reply.send(await query.handle());
+  });
 
-app.post('/clips/upload', async (req, reply) => {
-  const uploadedFile = await req.file();
-  const { filename, file } = uploadedFile;
-  const upload = new UploadClip(s3Service, clipsBucketName, filename.split(' ').join('-'), file);
-  reply.send(await upload.handle());
-});
+  app.post('/clips/upload', async (req, reply) => {
+    const uploadedFile = await req.file();
+    const { filename, file } = uploadedFile;
+    const upload = new UploadClip(s3Service, clipsBucketName, filename.split(' ').join('-'), file);
+    reply.send(await upload.handle());
+  });
+  return app;
+}
 
-app.listen(port, (err, address) => {
-  if (err) {
-    console.error(err);
-    process.exit(1);
-  }
-  console.log(`Server listening at ${address}`);
-})
+if (require.main === module) {
+  init().listen(port, err => {
+    if (err) {
+      console.error(err);
+    }
+  });
+} else {
+  module.exports = init;
+}
